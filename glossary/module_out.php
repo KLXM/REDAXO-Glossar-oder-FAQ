3f6a47e0baa44b2244fa09957ba84fbd7ce3ee16
Thomas Skerbis <skerbis@klxm.de>
2016-08-06 10:28:38 +0200

    Update module_out.php

--- glossary/module_out.php
+++ glossary/module_out.php
@@ -6,19 +6,18 @@ $query = "SELECT * FROM $db_table  ORDER BY Begriff ";
 $sql->setQuery($query, array($id));
 $counter=1;
 $bcounter=1;
-
+if (count($sql)) {
 foreach($sql as $row)
 {
-
-$id=$row->getValue("id");
-$begriff = $row->getValue("Begriff");
-$beschreibung = $row->getValue("beschreibung");
-# $beschreibung = nl2br($beschreibung); // wenn nur eine Textarea ohne WYSIWYG verwendet wird
+ $id=$row->getValue("id");
+ $begriff = $row->getValue("Begriff");
+ $beschreibung = $row->getValue("beschreibung");
+ # $beschreibung = nl2br($beschreibung); // wenn nur eine Textarea ohne WYSIWYG verwendet wird
  $counter++;
  if (strtoupper(substr($begriff,0,1)) != $dummy) { 
- $bcounter++;   
-$buchstabe ='<h2 id="buchstabe'.strtoupper(substr($begriff,0,1)).'">'.strtoupper(substr($begriff,0,1)). '</h2>'; 
-$index .= '<a type="button" class="btn btn-default scrollink" href="#buchstabe'.strtoupper(substr($begriff,0,1)).'">'.strtoupper(substr($begriff,0,1)). '</a>';   
+    $bcounter++;   
+    $buchstabe ='<h2 id="buchstabe'.strtoupper(substr($begriff,0,1)).'">'.strtoupper(substr($begriff,0,1)). '</h2>'; 
+    $index .= '<a type="button" class="btn btn-default scrollink" href="#buchstabe'.strtoupper(substr($begriff,0,1)).'">'.strtoupper(substr($begriff,0,1)). '</a>';   
  } 
  else {$buchstabe = "";}
  
@@ -38,4 +37,5 @@ $dummy = strtoupper(substr($begriff,0,1));
  } 
 echo $index;
 echo $out;
+}
 ?>
